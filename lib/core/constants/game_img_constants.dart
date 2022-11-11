b96eefeaaab416041998690a37c5728f930f1a00
yuksi-root <yukselcansimsek@gmail.com>
2022-11-11 23:57:30 +0300

    add image constants

---
+++ lib/core/constants/game_img_constants.dart
@@ -0,0 +1,118 @@
+class GameImgConstants {
+  static const String hiddenCardPng = 'assets/images/hidden.png';
+
+  static const String x1 = "assets/images/1.png";
+  static const String x2 = "assets/images/2.png";
+  static const String x3 = "assets/images/3.png";
+  static const String x4 = "assets/images/4.png";
+  static const String x5 = "assets/images/5.png";
+
+  static const String x6 = "assets/images/6.png";
+  static const String x7 = "assets/images/7.png";
+  static const String x8 = "assets/images/8.png";
+  static const String x9 = "assets/images/9.png";
+  static const String x10 = "assets/images/10.png";
+
+  static const String x11 = "assets/images/11.png";
+  static const String x12 = "assets/images/12.png";
+  static const String x13 = "assets/images/13.png";
+  static const String x14 = "assets/images/14.png";
+  static const String x15 = "assets/images/15.png";
+
+  static const String x16 = "assets/images/16.png";
+  static const String x17 = "assets/images/17.png";
+  static const String x18 = "assets/images/18.png";
+  static const String x19 = "assets/images/19.png";
+  static const String x20 = "assets/images/20.png";
+
+  static const String x21 = "assets/images/21.png";
+  static const String x22 = "assets/images/22.png";
+  static const String x23 = "assets/images/23.png";
+  static const String x24 = "assets/images/24.png";
+  static const String x25 = "assets/images/25.png";
+
+  static const String x26 = "assets/images/26.png";
+  static const String x27 = "assets/images/27.png";
+  static const String x28 = "assets/images/28.png";
+  static const String x29 = "assets/images/29.png";
+  static const String x30 = "assets/images/30.png";
+
+  static const String x31 = "assets/images/31.png";
+  static const String x32 = "assets/images/32.png";
+  static const String x33 = "assets/images/33.png";
+  static const String x34 = "assets/images/34.png";
+  static const String x35 = "assets/images/35.png";
+
+  static const String x36 = "assets/images/36.png";
+  static const String x37 = "assets/images/37.png";
+  static const String x38 = "assets/images/38.png";
+  static const String x39 = "assets/images/39.png";
+  static const String x40 = "assets/images/40.png";
+
+  static const String x41 = "assets/images/41.png";
+  static const String x42 = "assets/images/42.png";
+  static const String x43 = "assets/images/43.png";
+  static const String x44 = "assets/images/44.png";
+  static const String x45 = "assets/images/45.png";
+
+  static const String x46 = "assets/images/46.png";
+  static const String x47 = "assets/images/47.png";
+  static const String x48 = "assets/images/48.png";
+  static const String x49 = "assets/images/49.png";
+  static const String x50 = "assets/images/50.png";
+  static const String x51 = "assets/images/51.png";
+
+  static const List<String> imageList = [
+    x1,
+    x2,
+    x3,
+    x4,
+    x5,
+    x6,
+    x7,
+    x8,
+    x9,
+    x10,
+    x11,
+    x12,
+    x13,
+    x14,
+    x15,
+    x16,
+    x17,
+    x18,
+    x19,
+    x20,
+    x21,
+    x22,
+    x23,
+    x24,
+    x25,
+    x26,
+    x27,
+    x28,
+    x29,
+    x30,
+    x31,
+    x32,
+    x33,
+    x34,
+    x35,
+    x36,
+    x37,
+    x38,
+    x39,
+    x40,
+    x41,
+    x42,
+    x43,
+    x44,
+    x45,
+    x46,
+    x47,
+    x48,
+    x49,
+    x50,
+    x51,
+  ];
+}
