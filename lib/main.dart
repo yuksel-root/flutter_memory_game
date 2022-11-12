4873a48af8e12cf9f5f3c628a36f25061eead8dc
yuksi-root <yukselcansimsek@gmail.com>
2022-11-12 23:50:04 +0300

    update game logic functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_memory_game/view/home_view.dart';
+import 'package:flutter_memory_game/core/constants/navigation_constants.dart';
+import 'package:flutter_memory_game/core/navigation/navigation_route.dart';
+import 'package:flutter_memory_game/core/navigation/navigation_service.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_memory_game/view/home_view.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
+    SystemUiOverlay.bottom,
+  ]);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
@@ -12,14 +21,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    WidgetsFlutterBinding.ensureInitialized();
-
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
-      SystemUiOverlay.bottom,
-    ]);
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeView(),
+      home: const HomeView(),
+      onGenerateRoute: NavigationRoute.instance.generateRoute,
+      navigatorKey: NavigationService.instance.navigatorKey,
+      initialRoute: NavigationConstants.HOME_VIEW,
     );
   }
 }
