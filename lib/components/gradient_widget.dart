dfe77857a0dba616c309cb35d824aca7bdd1549f
yuksi-root <yukselcansimsek@gmail.com>
2022-11-23 23:59:24 +0300

    update gradient widget

---
+++ lib/components/gradient_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GradientWidget extends StatelessWidget {
+  const GradientWidget({
+    Key? key,
+    required this.gradient,
+    required this.widget,
+  }) : super(key: key);
+
+  final Widget widget;
+  final Gradient gradient;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      blendMode: BlendMode.srcATop,
+      shaderCallback: (bounds) => gradient.createShader(bounds),
+      child: widget,
+    );
+  }
+}
