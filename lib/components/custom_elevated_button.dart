7536d09ead6b779cdcb125c7691f661c3b5fb9cb
yuksi-root <yukselcansimsek@gmail.com>
2022-12-14 23:58:59 +0300

    add custom elevatedBtn

---
+++ lib/components/custom_elevated_button.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_memory_game/components/gradient_widget.dart';
+
+class CustomBtn extends StatelessWidget {
+  final Function onPressFunc;
+  final Gradient bgGradient;
+  final Gradient iconGradient;
+  final IconData iconName;
+  final BorderRadius borderRadius;
+  final double iconSize;
+  const CustomBtn({
+    Key? key,
+    required this.onPressFunc,
+    required this.bgGradient,
+    required this.iconGradient,
+    required this.iconName,
+    required this.borderRadius,
+    required this.iconSize,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return circleButton(context);
+  }
+
+  ElevatedButton circleButton(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.transparent),
+        shadowColor: MaterialStateProperty.all(Colors.transparent),
+        overlayColor: MaterialStateProperty.resolveWith<Color?>(
+          (Set<MaterialState> states) {
+            if (states.contains(MaterialState.hovered)) {
+              return Colors.transparent;
+            }
+            if (states.contains(MaterialState.pressed)) {
+              return Colors.transparent;
+            }
+            return null;
+          },
+        ),
+      ),
+      onPressed: () => onPressFunc(),
+      child: FittedBox(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: bgGradient,
+            borderRadius: borderRadius,
+          ),
+          child: GradientWidget(
+            gradient: iconGradient,
+            widget: Icon(
+              iconName,
+              size: iconSize,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
