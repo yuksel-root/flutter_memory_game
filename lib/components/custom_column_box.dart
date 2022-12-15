6dd31bd504da20506c497d571ef57a0e44393af8
yuksi-root <yukselcansimsek@gmail.com>
2022-12-15 23:53:19 +0300

    add custom columnBox widget

---
+++ lib/components/custom_column_box.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_memory_game/core/extensions/context_extensions.dart';
+
+class CustomColumnBox extends StatelessWidget {
+  final EdgeInsetsGeometry cPadding;
+  final Gradient bgGradient;
+  final Widget columnChild1;
+  final Widget columnChild2;
+
+  const CustomColumnBox({
+    Key? key,
+    required this.cPadding,
+    required this.bgGradient,
+    required this.columnChild1,
+    required this.columnChild2,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform(
+      transform: Matrix4.identity()..setEntry(3, 2, 0.002),
+      child: Container(
+        padding: cPadding,
+        child: Container(
+          // ignore: sort_child_properties_last
+          child: Container(
+            padding: cPadding,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: context.dynamicH(0.014),
+                ),
+                columnChild1,
+                SizedBox(
+                  height: context.dynamicH(0.014),
+                ),
+                columnChild2,
+              ],
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: const Color(0xff333333),
+            borderRadius: BorderRadius.circular(10),
+            gradient: bgGradient,
+            boxShadow: const [
+              BoxShadow(
+                color: Color(0xff799BA7),
+                offset: Offset(20.0, 20.0),
+                blurRadius: 30,
+                spreadRadius: 0.0,
+              ),
+              BoxShadow(
+                color: Color(0xff769AB0),
+                offset: Offset(-20.0, -20.0),
+                blurRadius: 30,
+                spreadRadius: 0.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
