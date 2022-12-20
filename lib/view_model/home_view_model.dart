0bcad157ce71346a251cae4eecfe4bd0f749e0d1
yuksi-root <yukselcansimsek@gmail.com>
2022-12-20 23:58:59 +0300

    update time bar ui widgets

---
+++ lib/view_model/home_view_model.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_memory_game/core/constants/navigation_constants.dart';
+import 'package:flutter_memory_game/core/navigation/navigation_service.dart';
+
+//-- HomeState Variables -- //
+enum HomeState {
+  empty,
+  loading,
+  completed,
+  error,
+}
+
+class HomeViewModel extends ChangeNotifier {
+  late NavigationService _navigation;
+  late HomeState? _state;
+
+  HomeViewModel() {
+    _navigation = NavigationService.instance;
+    _state = HomeState.empty;
+  }
+
+  void navigateToPageClear(
+    String path,
+  ) {
+    _navigation.navigateToPageClear(path: path, data: []);
+  }
+
+  void clickStageMode() {
+    navigateToPageClear(NavigationConstants.gameView);
+  }
+
+  void clickArcadeMode() {
+    navigateToPageClear(NavigationConstants.gameView);
+  }
+}
