ac3bd157f9e5db21b2aaf366f57363078fc98084
yuksi-root <yukselcansimsek@gmail.com>
2022-12-20 23:17:55 +0300

    update assets folder

--- lib/core/constants/game_img_constants.dart
+++ lib/core/constants/game_img_constants.dart
@@ -1,5 +1,5 @@
 class GameImgConstants {
-  static const String hiddenCardPng = 'assets/card_images/hidden.png';
-  static const String transparentPng = 'assets/card_images/tp.png';
-  static const String splashBg = 'assets/splash_images/sp2.jpg';
+  static const String hiddenCardPng = 'assets/game_card_png/hidden.png';
+  static const String transparentPng = 'assets/game_card_png/tp.png';
+  static const String splashBg = 'assets/splash_jpeg/sp2.jpeg';
 }
