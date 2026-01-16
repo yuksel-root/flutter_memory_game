792554ebfe31d6359e7911116fa6c76f683bb8c8
yuksel-root <yukselcansimsek@gmail.com>
2026-01-16 12:08:31 +0300

    updated web

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 set -e
 
-FLUTTER_VERSION="3.19.6"
-FLUTTER_DIR="$HOME/flutter"
 
-if [ ! -d "$FLUTTER_DIR" ]; then
-    echo "Downloading Flutter $FLUTTER_VERSION..."
-    curl -LO https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
-    tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz -C $HOME
-fi
-
-export PATH="$FLUTTER_DIR/bin:$PATH"
+export PATH="$HOME/flutter/bin:$PATH"
 
 # ---- Flutter web build ----
-flutter channel stable
-flutter upgrade
 flutter config --enable-web
 flutter pub get
-flutter build web --release
+flutter build web --release --no-wasm
 
 # ---- Netlify SPA redirect (CRITICAL) ----
 if [ -f web/_redirects ]; then
